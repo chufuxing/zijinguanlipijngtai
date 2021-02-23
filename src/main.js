e9db21b6ad0b7dab13f50a0c8bc51f7ca1a7e4cb
chufuxing <1229961873@qq.com>
2021-02-23 10:12:03 +0800

    add 测试

--- src/main.js
+++ src/main.js
@@ -2,12 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import ElementUI from 'element-ui'
-import 'element-ui/lib/theme-chalk/index.css'
-import 'lib-flexible'
+// import ElementUI from 'element-ui'
+// import 'element-ui/lib/theme-chalk/index.css'
+// import 'lib-flexible'
 Vue.config.productionTip = false
 
-Vue.use(ElementUI)
+// Vue.use(ElementUI)
 new Vue({
     render: h => h(App),
     router,
